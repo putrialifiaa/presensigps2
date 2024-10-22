6c9a5e540d9270f65c0bd83c98777912b7a1690d
putrialifiaa <putriralifiaa@gmail.com>
2024-10-22 10:31:47 +0700

    penomoran kode izin

--- app/Http/Controllers/IzinsakitController.php
+++ app/Http/Controllers/IzinsakitController.php
@@ -20,13 +20,25 @@ class IzinsakitController extends Controller
         $status = "s";
         $keterangan = $request->keterangan;
 
-        $kode_izin = rand(1,6);
+        $bulan = date("m",strtotime($tgl_izin_dari));
+        $tahun = date("Y",strtotime($tgl_izin_dari));
+        $thn = substr($tahun,2,2);
+        $lastizin = DB::table('pengajuan_izin')
+        ->whereRaw('MONTH(tgl_izin_dari)="'.$bulan.'"')
+        ->whereRaw('YEAR(tgl_izin_dari)="'.$tahun.'"')
+        ->orderBy('kode_izin','desc')
+        ->first();
+        $lastkodeizin = $lastizin != null ? $lastizin->kode_izin : "";
+        $format = "IZ".$bulan.$thn;
+        $kode_izin = buatkode($lastkodeizin,$format,3);
+
         if ($request->hasFile('sid')) {
             $sid = $kode_izin . "." . $request->file('sid')->getClientOriginalExtension();
         } else {
             $sid = null;
         }
         $data = [
+            'kode_izin' => $kode_izin,
             'nik' => $nik,
             'tgl_izin_dari' => $tgl_izin_dari,
             'tgl_izin_sampai' => $tgl_izin_sampai,
